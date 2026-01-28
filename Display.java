a1a5197cd4ad76892cb2848b7503f7eb832f0ffc
Ela Talipoğlu <ela.talipoglu@ug.bilkent.edu.tr>
2026-01-28 15:13:36 +0300

    feature

--- Display.java
+++ Display.java
@@ -1,5 +1,17 @@
-public class Display {
+import java.util.Scanner;
+public class Display{
     public static void main(String[] args) {
-        
+        Scanner scn = new Scanner(System.in);
+        int option = 0;
+        int arrSize;
+        System.out.print("Enter an array size: ");
+        arrSize = scn.nextInt();
+
+        while(option != 5){
+        System.out.print("1- Find Minimum \n2- Find Maximum \n3- Elements Differange From Average \n4- Sum With Odd Even Index \n 5- Exit \nChoose an option: ");
+        option = in.nextInt();
+        if()
+        }
+
     }
 }
