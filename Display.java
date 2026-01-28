c8394b890e74219d7ce380d4fd91e9468f50b2b7
Umut Sarıkaya <umutsarikaya@icloud.com>
2026-01-28 15:12:17 +0300

    Created Random array for use in other methods

--- Display.java
+++ Display.java
@@ -1,5 +1,13 @@
+import java.util.Random;
+
 public class Display {
     public static void main(String[] args) {
+        Random rng = new Random();
+
         
+        int[] array = new int[arrSize];
+        for (int i = 0 ; i < arrSize ; i++){
+            array[i] = rng.nextInt(0,101);
+        }
     }
 }
